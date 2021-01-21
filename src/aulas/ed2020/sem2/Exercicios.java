1955d86be792027170dad9d210bea947d91b2162
Francisco Rodrigues <frchico@gmail.com>
2021-01-21 15:39:20 -0300

    Resolução da questão 2

--- src/aulas/ed2020/sem2/Exercicios.java
+++ src/aulas/ed2020/sem2/Exercicios.java
@@ -1,15 +1,19 @@
 package aulas.ed2020.sem2;
 
-import java.util.Scanner;
-import java.io.FileWriter;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Exercicios {
     public static void main(String[] args){
-
+        try {
+            new Exercicios().Questao2();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    private final int qtdMax = 5;
+    public static final int QTD_MAX = 5;
     /*
     Ler um conjunto de valores em um vetor de inteiros (até 100 elementos)
      */
@@ -26,7 +30,21 @@ public class Exercicios {
     /*
     Escrever os valores inteiros em um arquivo (os 100 primeiros)
      */
-    public void Questao2(){
+    public void Questao2() throws IOException {
+        int[] dados = Questao1_LerAteXElementos(QTD_MAX);
+        File arquivo = new File("arquivoExemplo.txt");
 
+        if( arquivo.createNewFile()){
+            System.out.println("Arquivo criado.");
+        }
+        System.out.println("Caminho do arquivo: "+ arquivo.getAbsolutePath());
+        FileWriter arquivoWrite = new FileWriter(arquivo);
+        for (int i = 0; i < QTD_MAX; i++) {
+            Integer item = dados[i];
+            arquivoWrite.write(item.toString() + "\n");
+        }
+        arquivoWrite.close();
+
+        System.out.println("Procedimento concluído.");
     }
 }
